1f6d5c52d1d0cd28d2142185025ccb2304546966
Igor Daré <eusouigor10@gmail.com>
2025-07-10 12:18:06 -0300

    Projeto

---
+++ Trabalho_2_2/src/main/java/igorigor/trabalho_2_2/Trabalho_2_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package igorigor.trabalho_2_2;
+
+/**
+ *
+ * @author The Jarbas
+ */
+public class Trabalho_2_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
